13aab53594791a7e0ccc2bdb27ace7047205c20c
LD7l <desmetleonard@gmail.com>
2026-03-10 14:46:48 +0100

    push

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import gurobipy as gp
 from gurobipy import GRB
 
+m = gp.Model("test")
+x = m.addVar(vtype=GRB.BINARY, name="x")
+
+m.setObjective(x, GRB.MAXIMIZE)
+m.optimize()
+
+print(f"Optimal value: {m.objVal}")
+print(f"x: {x.x}")  
+
